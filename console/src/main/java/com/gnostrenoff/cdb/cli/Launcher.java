b1d8050e135c015f289ef179caabe947fee03249
gnostrenoff <g.nostrenoff@gmail.com>
2016-03-22 18:05:37 +0100

    fixed cli

---
+++ console/src/main/java/com/gnostrenoff/cdb/cli/Launcher.java
@@ -0,0 +1,20 @@
+package com.gnostrenoff.cdb.cli;
+
+/**
+ * The Class Launcher.
+ */
+public class Launcher {
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+
+    Listener listener = new Listener();
+    listener.listen();
+
+  }
+
+}
